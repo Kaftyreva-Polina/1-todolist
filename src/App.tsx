eeb077ba0a19f0e1909e6b5e2026c4ae2aebbcf3
KaftyrevaPolina <pkaft.13.8@gmail.com>
2023-01-30 15:34:27 +0300

    Add 2-lesson, remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,27 @@ export type FilterValuesType = "all" | "active" | "completed"
 
 function App() {
     const todoListTitle_1: string = "What to learn"
-    // const todoListTitle_2: string = "What to buy"
 
     const [tasks, setTasks] = useState<Array<TaskType>>([
         {id: 1, title: "HTML", isDone: true},
         {id: 2, title: "CSS", isDone: true},
         {id: 3, title: "JS/TS", isDone: false},
     ])
-    // const tasks = result[0]
-    // const setTasks = result[1]
-    // console.log(result)
+
     const removeTask = (taskId: number) => {
         setTasks(tasks.filter(task => task.id !== taskId))
         console.log(tasks)
     }
     useEffect(() => {
         console.log(tasks)
-    }, [tasks]) //Для синхронизации для устранения ассинхронизации useState
+    }, [tasks])
 
-    const [filter, setFilter] = useState<FilterValuesType>("all") //useState даст нам новый массив после выбора фильтрации
+    const [filter, setFilter] = useState<FilterValuesType>("all")
     const changeFilter = (filter: FilterValuesType) => {
         setFilter(filter)
-    } //Сразу добавляем в TodoListPropsType
+    }
 
-    const getFilteredTasksForRender = //функция сама отфильтрует по выбранным параметрам нужный список тасок
+    const getFilteredTasksForRender =
         (tasks: Array<TaskType>, filter: FilterValuesType): Array<TaskType> => {
             switch (filter) {
                 case "active":
@@ -41,25 +38,19 @@ function App() {
             }
         }
 
-    const filteredTaskForRender = getFilteredTasksForRender(tasks, filter) //конкретные таски и значение фильтра
-    // const tasks_2: Array<TaskType> = [
-    //     {id: 1, title: "Meat", isDone: true},
-    //     {id: 2, title: "Fruit", isDone: false},
-    //     {id: 3, title: "Veggies", isDone: false},
-    // ]
+    const filteredTaskForRender = getFilteredTasksForRender(tasks, filter)
 
     return (
         <div className="App">
             <Todolist
                 title={todoListTitle_1}
-                tasks={filteredTaskForRender} //Передаем не все таски, а только отильтрованные
+                tasks={filteredTaskForRender}
                 removeTask={removeTask}
-                changeFilter={changeFilter} // Передаем т к в App.tsx  такой тип записи
+                changeFilter={changeFilter}
             />
-            {/*<Todolist title={todoListTitle_2} tasks={tasks_2}/>*/}
         </div>
     );
 }
 
-//
+
 export default App;
